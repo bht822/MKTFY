923ff7db37f010a83f1573798f45ea2acb83065b
Duy <duy>
2020-12-03 01:55:45 -0700

    added controllers

--- MKTFY.App/ApplicationDbContext.cs
+++ MKTFY.App/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace MKTFY.App
         {
             
         }
-
+        
         public DbSet<Customer> Customers { get; set; }
         public DbSet <CustomerAddress> CustomerAddresses { get; set; }
         
